dcdb2e9c81d6aa83c21318d39a6aa9a0c3000de5
ryosuke tachibana <ryosuke@neuiterm2.local>
2015-10-23 16:52:07 +0900

    modified

--- git_dotfiles.sh
+++
@@ -1,8 +0,0 @@
-current=`pwd`
-cd ~/Git/dotfiles
-./copy_to_git.sh
-git add .
-git commit -m $1
-git push origin master
-cd $current
-
--- push_git_dotfiles.sh
+++ push_git_dotfiles.sh
@@ -5,4 +5,3 @@ git add .
 git commit -m $1
 git push origin master
 cd $current
-
