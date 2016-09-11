922336525208d750a4bccc1464efe3150a399044
ryosuke tachibana <ryosuke@neuiterm2.local>
2016-09-12 04:00:26 +0900

    test

--- copy_to_git.sh
+++ copy_to_git.sh
@@ -1,5 +1,5 @@
 cp -f ~/.vimrc ~/Git/dotfiles/.vimrc 
-cp -rf ~/.vim ~/Git/dotfiles/.vim
+cp -rf ~/.vim ~/Git/dotfiles/
 cp -f ~/.zshrc ~/Git/dotfiles/.zshrc
 cp -f ~/.tmux.conf ~/Git/dotfiles/.tmux.conf 
 cp -f ~/push_git_dotfiles.sh ~/Git/dotfiles/push_git_dotfiles.sh
